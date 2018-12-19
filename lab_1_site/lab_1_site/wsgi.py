5a32556b6a3671753e7772a6c1754e78489dbc30
AlVerb <demplar@yandex.ru>
2018-12-19 18:18:36 +0500

    fix wsgi.py

--- lab_1_site/lab_1_site/wsgi.py
+++ lab_1_site/lab_1_site/wsgi.py
@@ -1,16 +1,9 @@
-"""
-WSGI config for lab_1_site project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
-"""
-
 import os
-
+import sys
+path = os.path.expanduser('~/my_django_site')
+if path not in sys.path:
+    sys.path.append(path)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'lab_1_site.settings'
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lab_1_site.settings')
-
-application = get_wsgi_application()
+from django.contrib.staticfiles.handlers import StaticFilesHandler
+application = StaticFilesHandler(get_wsgi_application())
